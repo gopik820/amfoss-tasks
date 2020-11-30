c1b05c50fdd094d578f952847f56e3be917cf22d
gopik820 <73281663+gopik820@users.noreply.github.com>
2020-11-30 23:13:54 +0530

    Update spaming.js

--- task-09/spaming.js
+++ task-09/spaming.js
@@ -1,1 +1,12 @@
 
+timedelay = 1; 
+count= 10;
+i= 0;
+spaming = setInterval(function(){
+document.getElementsByClassName('composer_rich_textarea')[0].innerHTML = "Hello";
+$('.im_submit_send').trigger('mousedown');	
+i++;
+
+if( i  === count )
+clearInterval(spaming);
+} , timedelay * 1000 ) ;
